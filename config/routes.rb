6c225f42557ef4fd6c32cd812b69f7cc775d9543
Eric Sipple <saalon@gmail.com>
2014-10-08 10:05:13 -0400

    Fixing routes.rb for rails 3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
+
 TrustyCms::Application.routes.draw do
   namespace :admin do
     resources :sites do
-      get :remove, on: member
-      post :move_higher, on: member
-      post :move_lower, on: member
-      put :move_to_top, on: member
-      put :move_to_bottom, on: member
+      get :remove, on: :member
+      post :move_higher, on: :member
+      post :move_lower, on: :member
+      put :move_to_top, on: :member
+      put :move_to_bottom, on: :member
     end
   end
 end
