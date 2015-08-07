a4afd5d69a3a3ca59328412cbb829c43b8f2ce6c
Eric Sipple <saalon@gmail.com>
2015-08-07 07:53:58 -0400

    Adding necessary lines to the initializer file. These had been in the site project itself, but moving them here (provided it works) is the better place for them.

--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -1,4 +1,8 @@
 require 'multi_site/engine'
+
+Admin::LayoutsController.send :helper, MultiSite::SiteChooserHelper
+Admin::SnippetsController.send :helper, MultiSite::SiteChooserHelper
+
 TrustyCms.config do |config|
   # config.define "setting.name", :default => 'value', :select_from => ['foo', 'bar']
   require 'multi_site/scoped_validation'
